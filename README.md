6dc675e53078f16100643a2627ab9ddef6186bfe
Josh Dierker <joshuadierker@gmail.com>
2025-02-17 16:43:48 -0600

    updated readme, finalized health check, froze dependencies

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# HTTP-Health-Check
+# HTTP Endpoint Health Check
+A program to check the health of a set of HTTP endpoints given a list of HTTP endpoints in YAML format
+## Setup
+* Assumes Python 3.5 or later
+* Assumes UNIX-like CLI (eg bash, zsh)
+
+### 1. Create virtual environment and install dependencies
+    
+#### Example from base directory of repository:  
+
+    
+    python3 -m venv venv 
+    source venv/Scripts/activate
+    pip install -r requirements.txt
+
+    
+### 2. Run `healthcheck.py` and specify the path to the input YAML file
+
+#### Example from base directory of repository using sample input:
+
+    python3 healthcheck.py input.yaml
+
+#### Example from base directory of repository using generic YAML file for input:
+
+    python3 healthcheck.py <PATH_TO_YAML_FILE>
+
+### 3. To exit the program gracefully, use `CTRL+C`
+
+### 4. To exit the virtual environment, use `deactivate`
+
+
--- healthcheck.py
+++ healthcheck.py
@@ -6,32 +6,60 @@ import yaml
 
 
 def main():
+    # use argparse to get the path to input yaml
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "Path", help="Provide the path to the input YAML file.")
     args = parser.parse_args()
-    print(args.Path)
+    # open and ingest input yaml
     with open(args.Path, "r") as input:
-        dict = yaml.safe_load(input)
+        dict_list = yaml.safe_load(input)
     res = {}
     while True:
         try:
-            for d in dict:
-                # print(d.keys())
+            for d in dict_list:
                 key = urlparse(d["url"]).netloc
-                # tuple where 0th is up and 1st is down counts
+                # list where 0th is UP count and 1st is DOWN count
                 if key not in res.keys():
+                    # then we initialize an empty list to track UP and DOWN counts
                     res[key] = [0, 0]
 
                 if health_check(d):
+                    # Endpoint is UP
                     res[key][0] = res[key][0] + 1
                 else:
+                    # Endpoint is DOWN
                     res[key][1] = res[key][1] + 1
-            print(res)
-            # busy wait not ideal
+            # log current availability percentages to stdout
+            print_ap(res)
+            # busy wait 15 seconds (not ideal)
             time.sleep(15)
         except KeyboardInterrupt:
+            # allow user to CTRL+C to stop the execution
             break
+
+
+"""
+Helper function to calculate, format and print the availability percentages
+to standard out after each pass through the list of endpoints.
+"""
+
+
+def print_ap(map: dict):
+    for key in map.keys():
+        up = map[key][0]
+        down = map[key][1]
+        total = up + down
+        ap = 100 * (up / total)
+        print(f"{key} has {round(ap)}% availabilty percentage")
+
+
+"""
+Helper function that ensures the necessary components for the HTTP requests
+are initialized, makes the HTTP request, and determines whether or not the 
+response from the endpoint meets the criteria for UP (<500ms latency and a status
+code in [200,299]) or if the endpoint is DOWN
+"""
 
 
 def health_check(block: dict):
